e0678185eecc22a77a96e16dbc5fc22232eb8564
Jack Willis <jwillis@sprocketsecurity.com>
2024-04-03 23:36:29 -0500

    make a cool effect

--- sketches/wind.js
+++ sketches/wind.js
@@ -1,10 +1,12 @@
+const spacing = 27;
+const lineLength = 17;
+
 let angles = [];
-const spacing = 40; // Increase the spacing between lines
-const lineLength = 20; // Increase the length of the lines
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  // Initialize all angles and adjust spacing
+  createCanvas(windowWidth / 8, windowHeight / 8);
+  noSmooth();
+
   for (let i = 0; i < width; i += spacing) {
     angles[i] = [];
     for (let j = 0; j < height; j += spacing) {
@@ -14,20 +16,32 @@ function setup() {
 }
 
 function draw() {
-  background(240);
+  colorMode(HSL, 360, 90, 190);
 
-  // Display and update angles to simulate wind
+  if (Math.random() > 0.1) {
+    background(0);
+  }
+
+  strokeWeight(1);
+  strokeCap(SQUARE);
+
   for (let i = 0; i < angles.length; i += spacing) {
     for (let j = 0; j < angles[i].length; j += spacing) {
       push();
-      translate(i, j);
+
+      const xjitter = (Math.random() - 0.5) * 1.5;
+      const yjitter = (Math.random() - 0.5) * 1.5;
+      translate(i + xjitter, j + (Math.random()-0.5)*1.5);
       rotate(angles[i][j]);
-      stroke(0);
-      line(0, 0, lineLength, 0); // Increase line length
+
+      const hue = map(i, 0, width, 0, 360);
+      stroke(hue, 100, 100, 3);
+      line(0, 0, lineLength, 0);
       pop();
 
-      // Update the angle to simulate movement
-      angles[i][j] += 0.01;
+      const deltaAngularSpeed = 0.01 + Math.cos(millis() / 20000 + Math.sin(millis() / 76544) * 0.4)*0.02 + (Math.random()-0.5)*0.005;
+
+      angles[i][j] += deltaAngularSpeed;
     }
   }
 }
